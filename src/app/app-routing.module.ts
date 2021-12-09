72bd5ae908e7ec6dbe25cf9420292160bbe62a9d
Andy Chung <andy6756@gmail.com>
2021-12-09 15:42:07 +0800

    Update Html Content

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,6 +3,7 @@ import { NgModule, Component } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 
 import { HomeComponent } from './client/home/home.component';
+import { LoginComponent } from './admin/login/login.component';
 
 
 const routes: Routes = [
@@ -13,6 +14,10 @@ const routes: Routes = [
       {
         path: 'home',
         component: HomeComponent
+      },
+      {
+        path: 'login',
+        component: LoginComponent
       },
       {
         path: '',
