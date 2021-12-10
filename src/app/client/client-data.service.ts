fb4e8513d5793ab1cbaf34f3e1ed0474ed3fb1c4
Andy Chung <andy6756@gmail.com>
2021-12-10 14:27:32 +0800

    Get All Product and Display

--- src/app/client/client-data.service.ts
+++ src/app/client/client-data.service.ts
@@ -1,23 +1,41 @@
+import { map } from 'rxjs/operators';
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class ClientDataService {
 
+  errorMessage;
   apiPath: string;
   constructor(private http: HttpClient) {
     this.apiPath = "https://vue3-course-api.hexschool.io/api/andychung/products/all";
-
   }
 
   run(){
     console.log('client-data Service');
   }
 
-  getAllProduct(){
-    return this.http.get(this.apiPath);
+  getAllProduct():Observable<any>{
+    return this.http.get(this.apiPath).pipe(map(data => data));
   }
 
+}
+
+export interface AllProduct{
+  success: boolean;
+  products: Observable<Product>;
+}
+
+export interface Product{
+  category: string;
+  id: string;
+  image: string;
+  is_enabled: boolean;
+  origin_price: number;
+  price: number;
+  title: string;
+  unit: string;
 }
--- src/app/client/home/home.component.ts
+++ src/app/client/home/home.component.ts
@@ -1,6 +1,8 @@
-import { ClientDataService } from './../client-data.service';
+import { HttpClient } from '@angular/common/http';
+import { ClientDataService, AllProduct, Product } from './../client-data.service';
 import { Component, OnInit } from '@angular/core';
 import { Observable } from 'rxjs';
+
 
 @Component({
   selector: 'app-home',
@@ -9,25 +11,20 @@ import { Observable } from 'rxjs';
 })
 export class HomeComponent implements OnInit {
 
-  products$: Observable<any>;
-  detailData: any[] = [];
-  // arrayOfItems:Type[];
-  constructor(public datasvc: ClientDataService) {
+  productModel: AllProduct;
+  constructor(public datasvc: ClientDataService) {}
 
-    console.log('ready home data');
-    this.datasvc.getAllProduct().subscribe(result => {
-      // console.log('result=', result.products);
-      this.detailData.push(result);
-      // this.products$ = result;
-    });
-
-    // this.detailData.push(this.datasvc.getAllProduct());
-    this.products$ = this.datasvc.getAllProduct();
-   }
   ngOnInit() {
+    console.log('Init data');
+    this.getProducts();
+    console.log('Init data done');
   }
 
-
+  getProducts(){
+    this.datasvc.getAllProduct().subscribe(products => {
+      this.productModel = products;
+    });
+  }
 
 }
 
